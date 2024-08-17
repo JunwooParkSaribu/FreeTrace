cfc85acf8b42342c5b05cf92571fc264beec67fa
Junwoo PARK <junwoo.park@sorbonne-universite.fr>
2024-08-17 23:29:10 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+from Cython.Build import cythonize
+
+setup(
+    name='Hello world app',
+    ext_modules=cythonize(["./module/image_pad.pyx", "./module/regression.pyx"], language_level = "3", annotate=True),
+)
+
+#  python3 setup.py build_ext --inplace
