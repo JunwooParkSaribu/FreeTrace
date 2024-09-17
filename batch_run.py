f677336d8d129a05c5b2c8f2347c8edf13ec0aec
JunwooParkSaribu <jwoo@DESKTOP-CKS7USV>
2024-09-17 11:04:12 +0200

    diffusion map updated

--- batch_run.py
+++ batch_run.py
@@ -1,4 +1,11 @@
 import os
+import sys
+import subprocess
+
+
+def run_command(cmd):
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return process
 
 
 def write_config(filename):
@@ -47,3 +54,10 @@ for file in file_list:
             exec(f.read())
         with open("Tracking.py") as f:
             exec(f.read())
+        if os.path.exists('make_image.py'):
+            proc = run_command([sys.executable.split('/')[-1], f'make_image.py', f'./outputs/{file.strip().split(".tif")[0]}_traces.trxyt'])
+            proc.wait()
+            if proc.poll() == 0:
+                print(f'diffusion map -> successfully finished')
+            else:
+                print(f'diffusion map -> failed with status:{proc.poll()}')
