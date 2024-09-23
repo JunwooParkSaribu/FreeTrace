dd503b86c047bbefc2c1e0e1d22b5ebde412c495
JunwooParkSaribu <jwoo@DESKTOP-CKS7USV>
2024-09-23 11:59:14 +0200

    empty localization array in batch fixed

--- run.py
+++ run.py
@@ -1,4 +1,25 @@
+from FileIO import read_parameters
+import subprocess
+import os
+import sys
+
+
+def run_command(cmd):
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return process
+
+
+params = read_parameters('./config.txt')
+video_name = params['localization']['VIDEO']
+
 with open("Localization.py") as f:
     exec(f.read())
 with open("Tracking.py") as f:
     exec(f.read())
+if os.path.exists('make_image.py'):
+    proc = run_command([sys.executable.split('/')[-1], f'make_image.py', f'./outputs/{video_name.strip().split("/")[-1].split(".tif")[0]}_traces.trxyt'])
+    proc.wait()
+    if proc.poll() == 0:
+        print(f'diffusion map -> successfully finished')
+    else:
+        print(f'diffusion map -> failed with status:{proc.poll()}')
