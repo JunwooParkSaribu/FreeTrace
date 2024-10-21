ab0519f1b8f5d32f6732fde03dc40fd8d269a9a8
JunwooParkSaribu <junwoo5071@gmail.com>
2024-10-21 22:40:35 +0200

    requirements_installation

--- preinstallation.py
+++
@@ -1,5 +0,0 @@
-import sys
-import subprocess
-
-subprocess.run([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
-subprocess.run([sys.executable, 'cython_setup.py', 'build_ext', '--inplace'])
