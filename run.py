388a4b88b118753f307ad9cd6d3b2dfb6e2e9ab1
Junwoo PARK <junwoo.park@sorbonne-universite.fr>
2025-01-26 19:22:27 +0100

    updated

--- run.py
+++ run.py
@@ -1,29 +1,25 @@
 import os
 import sys
 from FreeTrace import Tracking, Localization
-from FreeTrace.module.FileIO import read_parameters, initialization
+from FreeTrace.module.FileIO import initialization
 
 
-"""
-Read configuration file.
-"""
-params = read_parameters('./config.txt')
-video_name = params['localization']['VIDEO']
-OUTPUT_DIR = params['localization']['OUTPUT_DIR']
+video_name = 'inputs/sample0.tiff'
+OUTPUT_DIR = 'outputs'
 
 
-WINSIZE = params['localization']['WINSIZE']
-THRES_ALPHA = params['localization']['THRES_ALPHA']
-DEFLATION_LOOP_IN_BACKWARD = params['localization']['DEFLATION_LOOP_IN_BACKWARD']
-SHIFT = params['localization']['SHIFT']
-SAVE_VIDEO_LOC = params['localization']['LOC_VISUALIZATION']
-LOC_GPU_AVAIL = params['localization']['GPU']
+WINSIZE = 7
+THRESHOLD = 1.0
+REAL_LOC = True
+SAVE_VIDEO_LOC = False
+LOC_GPU_AVAIL = True
 
 
-TIME_FORECAST = params['tracking']['TIME_FORECAST']
-CUTOFF = params['tracking']['CUTOFF']
-SAVE_VIDEO_TRACK = params['tracking']['TRACK_VISUALIZATION']
-TRACK_GPU_AVAIL = params['tracking']['GPU']
+TIME_FORECAST = 5
+CUTOFF = 2
+SAVE_VIDEO_TRACK = False
+REAL_TRACK = True
+TRACK_GPU_AVAIL = True
 
 
 if __name__ == "__main__":
@@ -33,18 +29,13 @@ if __name__ == "__main__":
         loc = False
         track = False
         initialization(False, verbose=False, batch=False)
-        """
         loc = Localization.run_process(input_video_path=video_name, output_path=OUTPUT_DIR,
-                                       window_size=WINSIZE, threshold=THRES_ALPHA,
-                                       deflation=DEFLATION_LOOP_IN_BACKWARD, shift=SHIFT,
-                                       gpu_on=LOC_GPU_AVAIL, save_video=SAVE_VIDEO_LOC, realtime_visualization=True, verbose=1, batch=False)
+                                       window_size=WINSIZE, threshold=THRESHOLD,
+                                       gpu_on=LOC_GPU_AVAIL, save_video=SAVE_VIDEO_LOC, realtime_visualization=REAL_LOC, verbose=1, batch=False)
         if loc:
-        """
-        track = Tracking.run_process(input_video_path=video_name, output_path=OUTPUT_DIR,
-                                        time_forecast=TIME_FORECAST, cutoff=CUTOFF,
-                                        gpu_on=TRACK_GPU_AVAIL, save_video=SAVE_VIDEO_TRACK, realtime_visualization=True, verbose=1, batch=False)
+            track = Tracking.run_process(input_video_path=video_name, output_path=OUTPUT_DIR,
+                                            time_forecast=TIME_FORECAST, cutoff=CUTOFF,
+                                            gpu_on=TRACK_GPU_AVAIL, save_video=SAVE_VIDEO_TRACK, realtime_visualization=REAL_TRACK, verbose=1, batch=False)
             
     except Exception as e:
         sys.exit(f'Err code:{e} on file:{video_name}')
-
-
