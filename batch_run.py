4c638228f9754ed52200be787c80662b2d13b86b
Junwoo PARK <junwoo.park@sorbonne-universite.fr>
2025-01-26 19:24:22 +0100

    run multiple files

--- batch_run.py
+++ batch_run.py
@@ -3,39 +3,34 @@ import sys
 from datetime import datetime
 from tqdm import tqdm
 from FreeTrace import Tracking, Localization
-from FreeTrace.module.FileIO import initialization, read_parameters
+from FreeTrace.module.FileIO import initialization
 
 
-"""
-Read configuration file.
-"""
-batch_folder = 'inputs'
-params = read_parameters('./config.txt')
-OUTPUT_DIR = params['localization']['OUTPUT_DIR']
+input_folder = 'inputs'
+OUTPUT_DIR = 'outputs'
+
+WINSIZE = 7
+THRESHOLD = 1.0
+REAL_LOC = True
+SAVE_VIDEO_LOC = False
+LOC_GPU_AVAIL = True
 
 
-WINSIZE = params['localization']['WINSIZE']
-THRES_ALPHA = params['localization']['THRES_ALPHA']
-DEFLATION_LOOP_IN_BACKWARD = params['localization']['DEFLATION_LOOP_IN_BACKWARD']
-SHIFT = params['localization']['SHIFT']
-SAVE_VIDEO_LOC = params['localization']['LOC_VISUALIZATION']
-LOC_GPU_AVAIL = params['localization']['GPU']
-
-
-TIME_FORECAST = params['tracking']['TIME_FORECAST']
-CUTOFF = params['tracking']['CUTOFF']
-SAVE_VIDEO_TRACK = params['tracking']['TRACK_VISUALIZATION']
-TRACK_GPU_AVAIL = params['tracking']['GPU']
+TIME_FORECAST = 5
+CUTOFF = 2
+SAVE_VIDEO_TRACK = False
+REAL_TRACK = True
+TRACK_GPU_AVAIL = True
 
 
 if __name__ == "__main__":
-    if not os.path.isdir(batch_folder):
-        sys.exit(f'{batch_folder} is not a directory containing files')
+    if not os.path.isdir(input_folder):
+        sys.exit(f'{input_folder} is not a directory containing files')
     else:
         failed_tasks = []
         if not os.path.exists(f'{OUTPUT_DIR}'):
             os.makedirs(f'{OUTPUT_DIR}')
-        file_list = os.listdir(f'{batch_folder}')
+        file_list = os.listdir(f'{input_folder}')
         print(f'\n*****  Batch processing on {len(file_list)} files. ({len(file_list)*2} tasks: Localizations + Trackings)  *****')
         initialization(gpu=True, verbose=True, batch=True)
         PBAR = tqdm(total=len(file_list)*2, desc="Batch", unit="task", ncols=120, miniters=1)
@@ -46,15 +41,16 @@ if __name__ == "__main__":
             if file.strip().split('.')[-1] == 'tif' or file.strip().split('.')[-1] == 'tiff':
                 PBAR.set_postfix(File=file, refresh=True)
                 try:
-                    loc = Localization.run_process(input_video_path=f'{batch_folder}/{file}', output_path=OUTPUT_DIR,
-                                                   window_size=WINSIZE, threshold=THRES_ALPHA,
-                                                   deflation=DEFLATION_LOOP_IN_BACKWARD, shift=SHIFT,
-                                                   gpu_on=LOC_GPU_AVAIL, save_video=SAVE_VIDEO_LOC, verbose=0, batch=True)
+                    loc = Localization.run_process(input_video_path=f'{input_folder}/{file}', output_path=OUTPUT_DIR,
+                                                   window_size=WINSIZE, threshold=THRESHOLD,
+                                                   gpu_on=LOC_GPU_AVAIL, save_video=SAVE_VIDEO_LOC,
+                                                   realtime_visualization=REAL_LOC, verbose=0, batch=True)
                     PBAR.update(1)
                     if loc:
-                        track = Tracking.run_process(input_video_path=f'{batch_folder}/{file}', output_path=OUTPUT_DIR,
+                        track = Tracking.run_process(input_video_path=f'{input_folder}/{file}', output_path=OUTPUT_DIR,
                                                      time_forecast=TIME_FORECAST, cutoff=CUTOFF,
-                                                     gpu_on=TRACK_GPU_AVAIL, save_video=SAVE_VIDEO_TRACK, verbose=0, batch=True)
+                                                     gpu_on=TRACK_GPU_AVAIL, save_video=SAVE_VIDEO_TRACK, 
+                                                     realtime_visualization=REAL_TRACK, verbose=0, batch=True)
                     PBAR.update(1)
 
                 except Exception as e:
