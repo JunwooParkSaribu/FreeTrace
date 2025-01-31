ba9e177f2cf3e64313d470955543a359fd35da22
Junwoo PARK <junwoo.park@sorbonne-universite.fr>
2025-02-01 00:10:34 +0100

    updated

--- FreeTrace/module/ImageModule.py
+++ FreeTrace/module/ImageModule.py
@@ -79,7 +79,7 @@ class RealTimePlot(tk.Tk):
         except Exception as e:
             print(f'')
             print(f'FreeTrace turns off the real-time viusualization if it waits more than [{self.video_wait_max_time}s] or [{e}], to save the computational resources.')
-            print(f'FreeTrace is still running if you have still non-inferred frames. Please don\'t shut down, it is just slowed down due to high number of particles / resolution.')
+            print(f'FreeTrace is running if you have still non-inferred frames. Please don\'t shut down, it is just slowed down due to high number of particles / resolution.')
             print(f'')
             self.clean_tk_widgets()
 
