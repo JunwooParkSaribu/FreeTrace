8428c368f02146af7a568b52096a1fecd2d1a309
JunwooParkSaribu <junwoo@junwoo.>
2025-02-25 16:58:16 +0100

    cropping updated

--- crop.py
+++ crop.py
@@ -1,86 +1,86 @@
 import sys
 import os
 import numpy as np
+import cv2
+from roifile import ImagejRoi
 from FreeTrace.module.FileIO import read_trajectory, write_trajectory
+from FreeTrace.module.ImageModule import make_whole_img
 
 
 """
 crop image with 
-1. ROI(start x, start y, end x, end y)
-2. Frames(start frame, end frame)
+1. ROI (myroi.roi)
+2. Frames (start frame, end frame)
 """
-def crop_ROI_and_frame(csv_file, roi_input, frame_input):
-    start_x, start_y, end_x, end_y = roi_input
-    start_frame, end_frame = frame_input
+def crop_ROI_and_frame(csv_file, contours, start_frame, end_frame, crop_comparison):
     filtered_trajectory_list = []
     trajectory_list = read_trajectory(csv_file)
+    xmin = 999999
+    xmax = -1
+    ymin = 999999
+    ymax = -1
 
     for trajectory in trajectory_list:
+        skip = 0
         xyz = trajectory.get_positions()
         times = trajectory.get_times()
-        x = xyz[:, 0]
-        y = xyz[:, 1]
-        z = xyz[:, 2]
-        if start_x is not None:
-            x_cond = np.sum((x >= start_x) * (x<=end_x)) == len(x)
-            y_cond = np.sum((y >= start_y) * (y<=end_y)) == len(y)
-        if start_x is not None and start_frame is not None:
-            if x_cond and y_cond and times[0] >= start_frame and times[-1] <= end_frame:
-                filtered_trajectory_list.append(trajectory)
-        elif start_x is not None and start_frame is None:
-            if x_cond and y_cond:
-                filtered_trajectory_list.append(trajectory)
-        elif start_x is None and start_frame is not None:
-            if times[0] >= start_frame and times[-1] <= end_frame:
-                filtered_trajectory_list.append(trajectory)
-        else:
-            filtered_trajectory_list.append(trajectory)
+        xs = xyz[:, 0]
+        ys = xyz[:, 1]
+        zs = xyz[:, 2]
+        xmin = min(np.min(xs), xmin)
+        ymin = min(np.min(ys), ymin)
+        xmax = max(np.max(xs), xmax)
+        ymax = max(np.max(ys), ymax)
 
-    print(f'cropping info: ROI[{start_x}, {start_y}, {end_x}, {end_y}],  Frame:[{start_frame}, {end_frame}]')
+        for x, y in zip(xs, ys):
+            masked = cv2.pointPolygonTest(contours, (x, y), False)
+            if masked == -1:
+                skip = 1
+                break
+        
+        if skip == 1:
+            continue
+
+        if times[0] >= start_frame and times[-1] <= end_frame:
+            filtered_trajectory_list.append(trajectory)
+    print(xmin, xmax, ymin, ymax)
+    print(f'cropping info: ROI[{ROI_FILE}],  Frame:[{start_frame}, {end_frame}]')
     print(f'Number of trajectories before filtering:{len(trajectory_list)}, after filtering:{len(filtered_trajectory_list)}')
     write_trajectory(f'{".".join(csv_file.split("traces.csv")[:-1])}cropped_traces.csv', filtered_trajectory_list)
 
+    if crop_comparison:
+        print("*** Crop comparison image generated ***")
+        dummy_stack = np.empty((1, int(ymax+1), int(xmax+1), 1))
+        make_whole_img(trajectory_list, output_dir=f'{".".join(csv_file.split("traces.csv")[:-1])}before_crop.png', img_stacks=dummy_stack)
+        make_whole_img(filtered_trajectory_list, output_dir=f'{".".join(csv_file.split("traces.csv")[:-1])}after_crop.png', img_stacks=dummy_stack)
+
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        sys.exit('Example of command: python3 crop.py video_traces.csv')
-    csv_file = sys.argv[1].strip()
+    if not(len(sys.argv) == 3 or len(sys.argv) == 5 or len(sys.argv) == 6):
+        print('Example of command 1: python3 crop.py video_traces.csv roi_file.roi')
+        sys.exit('Example of command 2: python3 crop.py video_traces.csv roi_file.roi start_frame end_frame')
+    
+    csv_file = sys.argv[1].strip()  
+    roi_filename = sys.argv[2].strip()
+    if len(sys.argv) == 3:
+        start_frame = -1
+        end_frame = 9999999
+        crop_comparison = False
+    elif len(sys.argv) == 5:
+        start_frame = int(eval(sys.argv[3].strip()))
+        end_frame = int(eval(sys.argv[4].strip()))
+        crop_comparison = False
+    else:
+        start_frame = int(eval(sys.argv[3].strip()))
+        end_frame = int(eval(sys.argv[4].strip()))
+        crop_comparison = True
+
+    global ROI_FILE
+    ROI_FILE = roi_filename
+    contours = ImagejRoi.fromfile(ROI_FILE).coordinates().astype(np.int32)
+
     if not os.path.exists(csv_file):
         sys.exit(f'Input file: {csv_file} is not exist. please check again')
     print(f"input file name: {csv_file}")
 
-    while True:
-        roi_input =input(f'Crop with ROI in pixel: start x, start y, end x, end y\nexample of use: 10,10,25,30\nyour ROI input: ')
-        roi_input = roi_input.strip().split(',')
-        if len(roi_input) == 1 and roi_input[0] == '':
-            print('*** skip crop with ROI ***')
-            roi_input = [None, None, None, None]
-            break
-        if len(roi_input) != 4:
-            print('input is wrong, please enter 4 integers in pixel.\n')
-            continue
-        for i in range(4):
-            roi_input[i] = int(eval(roi_input[i]))
-        if roi_input[0] >= roi_input[2] or roi_input[1] >= roi_input[3]:
-            print('input is wrong, end x or end y must be greater than start x or start y.\n')
-            continue
-        break
-    print('')
-    while True:
-        frame_input =input(f'Crop with frame: start frame, end frame\nexample of use: 100,500\nyour frame input: ')
-        frame_input = frame_input.strip().split(',')
-        if len(frame_input) == 1 and frame_input[0] == '':
-            print('*** skip crop with frames ***')
-            frame_input = [None, None]
-            break
-        if len(frame_input) != 2:
-            print('input is wrong, please enter 2 frame numbers.\n')
-            continue
-        for i in range(2):
-            frame_input[i] = int(eval(frame_input[i]))
-        if frame_input[0] >= frame_input[1]:
-            print('input is wrong, end frame must be greater than start frame.\n')
-            continue
-        break
-
-    crop_ROI_and_frame(csv_file, roi_input, frame_input)
+    crop_ROI_and_frame(csv_file, contours, start_frame, end_frame, crop_comparison)
