c4b1e375d337d17623a40e172d3316a3cb3828ba
Junwoo PARK <junwoo.park@sorbonne-universite.fr>
2025-11-30 14:46:04 +0100

    updated

--- manip_data.py
+++ manip_data.py
@@ -2,7 +2,7 @@
 Manipulation of result data including cropping, remake of video with upscaling and etc.
 """
 
-from FreeTrace.module.image_module import make_loc_radius_video, remake_visual_trajectories, remake_visual_localizations, make_loc_depth_image, make_loc_radius_video_batch
+from FreeTrace.module.image_module import make_loc_radius_video, remake_visual_trajectories, remake_visual_localizations, make_loc_depth_image, make_loc_radius_video_batch, make_loc_radius_video_batch2
 from FreeTrace.module.auxiliary import crop_trace_roi_and_frame
 
 
@@ -27,14 +27,25 @@ if __name__ == '__main__':
     make_loc_radius_video(output_path, images, localization_file, frame_cumul=10, radius=[3, 25], start_frame=0, end_frame=100, alpha1=0.65, alpha2=0.35)
     """
 
-    
+    """
     output_path = 'outputs'
     image_list = ['inputs/sample0.tiff']
     localization_file_list = ['outputs/sample0_traces.csv']
     start_end_frames_for_each_file = [(1, 100)]
     make_loc_radius_video_batch(output_path, image_list, localization_file_list, frame_cumul=1000, radius=[3, 13],
                                 frame_list=start_end_frames_for_each_file, nb_min_particles=100, max_density=None, color='jet', alpha1=0.65, alpha2=0.35)
-    
+    """
+
+    """
+    output_path = 'outputs'
+    image_list = ['inputs/sample0.tiff']
+    localization_file_list = ['outputs/sample0_loc.csv']
+    trajectory_file_list = ['outputs/sample0_traces.csv']
+    start_end_frames_for_each_file = [(1, 100)]
+    make_loc_radius_video_batch2(output_path, image_list, localization_file_list, trajectory_file_list, frame_cumul=1000, radius=[3, 13],
+                                 frame_list=start_end_frames_for_each_file, nb_min_particles=100, max_density=None, color='jet', alpha1=0.65, alpha2=0.35)
+    """
+
     """
     output_path = 'outputs'
     images = 'inputs/sample0.tiff'
