60d83a0e3fbcea88275d50221897b345ceaca61e
Junwoo PARK <junwoo.park@sorbonne-universite.fr>
2025-12-03 14:23:28 +0100

    imaging function added

--- manip_data.py
+++ manip_data.py
@@ -2,7 +2,7 @@
 Manipulation of result data including cropping, remake of video with upscaling and etc.
 """
 
-from FreeTrace.module.image_module import make_loc_radius_video, remake_visual_trajectories, remake_visual_localizations, make_loc_depth_image, make_loc_radius_video_batch, make_loc_radius_video_batch2
+from FreeTrace.module.image_module import make_loc_radius_video, remake_visual_trajectories, remake_visual_localizations, make_loc_depth_image, make_loc_radius_video_batch, make_loc_radius_video_batch2, make_diffusion_map
 from FreeTrace.module.auxiliary import crop_trace_roi_and_frame
 
 
@@ -14,11 +14,13 @@ if __name__ == '__main__':
     make_loc_depth_image(output_path, coords_path, multiplier=4, winsize=7, resolution=2, dim=3)
     """
     
+
     """
     trace_path = 'outputs/sample0_traces.csv'
     roi_path = ''
     crop_trace_roi_and_frame(trace_path, roi_path, start_frame=1, end_frame=50)
     """
+
 
     """
     output_path = 'outputs'
@@ -26,6 +28,7 @@ if __name__ == '__main__':
     localization_file = 'outputs/sample0_loc.csv'
     make_loc_radius_video(output_path, images, localization_file, frame_cumul=10, radius=[3, 25], start_frame=0, end_frame=100, alpha1=0.65, alpha2=0.35)
     """
+
 
     """
     output_path = 'outputs'
@@ -35,6 +38,7 @@ if __name__ == '__main__':
     make_loc_radius_video_batch(output_path, image_list, localization_file_list, frame_cumul=1000, radius=[3, 13],
                                 frame_list=start_end_frames_for_each_file, nb_min_particles=100, max_density=None, color='jet', alpha1=0.65, alpha2=0.35)
     """
+
 
     """
     output_path = 'outputs'
@@ -46,6 +50,7 @@ if __name__ == '__main__':
                                  frame_list=start_end_frames_for_each_file, nb_min_particles=100, max_density=None, color='jet', alpha1=0.65, alpha2=0.35)
     """
 
+
     """
     output_path = 'outputs'
     images = 'inputs/sample0.tiff'
@@ -53,4 +58,11 @@ if __name__ == '__main__':
     trajectory_file = 'outputs/sample0_traces.csv'
     remake_visual_localizations(output_path, localization_file, images, start_frame=1, end_frame=100, upscaling=1)
     remake_visual_trajectories(output_path, trajectory_file, images, start_frame=1, end_frame=100, upscaling=1)
+    """
+    
+
+    """
+    output_path = 'outputs'
+    trajectory_file = 'outputs/sample0_traces.csv'
+    make_diffusion_map(output_path, trajectory_file, cutoff=0, diffusion_coef_bound=[0.01, 10], pixel_shape=(2048, 2048), zoom_amplifier=20, cmap_color='jet', thickness=1)
     """
